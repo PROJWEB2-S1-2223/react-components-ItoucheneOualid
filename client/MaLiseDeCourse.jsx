015e0eb7d18bed243f1278d4ad8eee76dbbb772d
ItoucheneOualid <116847305+ItoucheneOualid@users.noreply.github.com>
2023-01-09 10:38:24 +0000

    modification

---
+++ client/MaLiseDeCourse.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+  const MaListeDeCourse  =[
+    {
+
+      title:"commande1",
+      course="course1",
+    }
+{
+  title:"commande2",
+  course="course2",
+}
+
+{
+
+
+    title:"commande3",
+    course="course3",
+
+}
+
+
+  ]
+  function list();
+
+  return (
+  <UL>{
+      MaListeDeCourse.map(function affiche MaListeDeCourse(course);
+      return (
+
+        <li key=
+      )
+
+
+
+
+       } </UL>
+
+  );
+}
+
+export default App;
+
--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import './App.css';
+import MalisDeCourse from '.MaliseCurse/;
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-   
+
   );
 }
 
